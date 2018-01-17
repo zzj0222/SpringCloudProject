5bd2fc91bdd70b4601b09de578ffbb71f7289d15
zzj0222 <zzj19870301@163.com>
2018-01-17 13:54:36 +0800

    位置

--- laiai-spring-cloud-microservice/laiai-cloud-config/src/main/java/com/laiai/LaiaiConfigApplication.java
+++ laiai-spring-cloud-microservice/laiai-cloud-config/src/main/java/com/laiai/LaiaiConfigApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 /**
- * 配置中心启动类
+ * 配置中心启动类-服务端
  */
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 @EnableConfigServer
 public class LaiaiConfigApplication {
